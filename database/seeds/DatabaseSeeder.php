bdff411e5b06898de7793cd7c4d8433c1a146a5b
vagrant <vagrant@homestead>
2019-12-16 20:21:55 +0000

    Edit form

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 5)->create()->each(function($user){
+        factory(App\User::class, 10)->create()->each(function($user){
             $user->questions()
                 ->saveMany(
-                    factory(App\Question::class,rand(1,5))->make()
+                    factory(App\Question::class,rand(1,3))->make()
                 );
         });
         
