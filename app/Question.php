78e2443e5fb1bd4b0b4f32749078ddc448a484d9
vagrant <vagrant@homestead>
2019-12-16 06:16:48 +0000

    Fix

--- app/Question.php
+++ app/Question.php
@@ -38,4 +38,18 @@ class Question extends Model
     public function getCreatedDateAttribute(){
         return $this->created_at->format("d/m/Y");
     }
+
+    /**
+     * check the status of eaach question
+     */
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
 }
